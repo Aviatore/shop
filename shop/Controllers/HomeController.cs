5a279124810988436f5f2b85f65ddc098a673587
Wojciech Wesołowski <wojtek.wes@gmail.com>
2021-02-10 17:40:58 +0100

    Add helper function to execute raw sql queries

--- shop/Controllers/HomeController.cs
+++ shop/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using shop.Data;
 using shop.Models;
 
 namespace shop.Controllers
