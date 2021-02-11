76b4e7729048cff1d7957e52698f6f461efc48a7
Ewa Zelenska <ewa.zelenska@gmail.com>
2021-02-11 11:54:42 +0100

    ShoppingCartViewModel.cs implemented: - list of OrderedBook.cs (bookid, amount, price) - add/ remove - total price/ total amount

--- shop/Models/OrderedBook.cs
+++ shop/Models/OrderedBook.cs
@@ -4,12 +4,14 @@ namespace shop.Models
     {
         public int BookId { get; set; }
         public int Amount { get; set; }
+        public string Title { get; set; }
         
         public double Price { get; set; }
 
         public OrderedBook(Book book)
         {
             BookId = book.BookId;
+            Title = book.Title;
             Amount = 1;
             Price = book.Price;
         }
