1373ca94766a4e89360956182f9227503a892cac
Wojciech Wesołowski <wojtek.wes@gmail.com>
2021-02-11 21:56:53 +0100

    Modify the Address model

---
+++ shop/Migrations/20210211204403_addressAddZipCode.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace shop.Migrations
+{
+    public partial class addressAddZipCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "zipcode",
+                table: "addresses",
+                type: "varchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "zipcode",
+                table: "addresses");
+        }
+    }
+}
--- shop/Models/Address.cs
+++ shop/Models/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
@@ -12,11 +13,12 @@ namespace shop.Models
             OrderBillingAddresses = new HashSet<Order>();
             OrderShippingAddresses = new HashSet<Order>();
         }
-
+        
         public int AddressId { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public string ZipCode { get; set; }
 
         public virtual ICollection<Order> OrderBillingAddresses { get; set; }
         public virtual ICollection<Order> OrderShippingAddresses { get; set; }
